5f555da46c80545c4374dfedca53738f68e56c66
pavan <pavank.d@gmail.com>
2020-03-08 22:16:55 +0530

    first commit with components added

---
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,28 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BinarySearchImpl {
+	@Autowired
+	private SortAlgorithm sortAlgorithm;
+
+
+	public void setSortAlgorithm(SortAlgorithm sortAlgorithm) {
+		this.sortAlgorithm = sortAlgorithm;
+	}
+
+
+
+
+
+
+	public int binarySearch(int[] numbers,int numberToSearch) {
+		int[] sortedNumber = sortAlgorithm.sort(numbers);
+		System.out.println(sortedNumber);
+		//search the array
+		return 3;
+	}
+
+}
---
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -0,0 +1,21 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringIn5StepsApplication {
+
+	public static void main(String[] args) {
+		//BinarySearchImpl binarySearchImpl = new BinarySearchImpl(new BubbleSortAlgorithm());
+		
+		
+		ApplicationContext applicationContext =   SpringApplication.run(SpringIn5StepsApplication.class, args);
+		BinarySearchImpl binarySearchImpl = applicationContext.getBean(BinarySearchImpl.class);
+		
+		int result = binarySearchImpl.binarySearch(new int[] {12,4,6}, 3);
+		System.out.println(result);
+	}
+
+}
